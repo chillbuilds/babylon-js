fc365b1800096e1ebcf0c60fbad1fc843811c702
chillbuilds <will@chillbuilds.com>
2020-05-03 17:58:43 -0700

    I made a box :)

---
+++ assets/scripts/index.js
@@ -0,0 +1,33 @@
+window.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById('babylonCanvas')
+    var engine = new BABYLON.Engine(canvas, true)
+
+    function createScene() {
+        var scene = new BABYLON.Scene(engine)
+        scene.clearColor = new BABYLON.Color3.Blue()
+        var box = BABYLON.Mesh.CreateBox("Box", 4.0, scene)
+        var camera = new BABYLON.ArcRotateCamera("arcCamera1",
+            BABYLON.Tools.ToRadians(45),
+            BABYLON.Tools.ToRadians(45),
+            10.0, box.position, scene)
+
+        camera.setTarget(BABYLON.Vector3.Zero())
+        camera.attachControl(canvas, true)
+
+        var light = new BABYLON.PointLight('pointLight', new BABYLON.Vector3(0,15,0),scene)
+        light.parent = camera
+        light.diffuse = new BABYLON.Color3(.5,.5,.5)
+
+        var material = new BABYLON.StandardMaterial("material", scene)
+        material.diffuseTexture = new BABYLON.Texture('../images/texture.jpg', scene)
+        box.material = material
+
+        return scene
+    }
+    var scene = createScene()
+    engine.runRenderLoop(function(){
+        // var material = scene.getMeshByName('Box').material
+
+        scene.render()
+    })
+})
